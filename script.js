8ab0d04e43309f33154b8a8c0d102aecccfe62f4
Iain <iain@iainferguson.org>
2020-06-05 20:05:29 +0100

    added cell coord detection linking into table array

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// const gameBoard = () => {
-//   let table = [
-//     ["", "", ""],
-//     ["", "", ""],
-//     ["", "", ""],
-//   ];
-// };
+let table = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
 
 // const player = (name) => {
 //   const getName = () => name;
@@ -13,3 +11,30 @@
 // const gamePlay = () => {};
 
 const gameBoard = document.querySelectorAll(".square");
+const playButton = document.getElementById("startGame");
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
+let turn = 1;
+
+function playGame() {
+  gameBoard.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      function getRowCellPosition(cell, move) {
+        row = cell.parentNode.rowIndex;
+        cell = cell.cellIndex;
+        table[row][cell] = move;
+      }
+      if (turn % 2 == 0) {
+        element.innerHTML = "X";
+        getRowCellPosition(element, "X");
+        turn++;
+      } else {
+        element.innerHTML = "O";
+        getRowCellPosition(element, "O");
+        turn++;
+      }
+    });
+  });
+}
+
+playButton.addEventListener("click", playGame);
